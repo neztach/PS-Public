a62b4d8dfcb181630f50aebeb87a66bb01160384
neztach <36271369+neztach@users.noreply.github.com>
2022-12-02 15:06:03 -0600

    Create Get-NonWinSvc.ps1

---
+++ Get-NonWinSvc.ps1
@@ -0,0 +1,54 @@
+### Get Non-Windows Services
+Function Get-NonWinSvc {
+    Param ($CN = 'localhost')
+
+    If ($CN) {
+        $toReturn = Invoke-Command -ComputerName $CN -ScriptBlock {
+            $NonDefaultServices = Get-wmiobject -Class Win32_Service | Where-Object {
+                $_.Caption -notmatch 'Windows' -and $_.PathName -notmatch 'Windows' -and 
+                $_.PathName -notmatch 'policyhost.exe' -and $_.Name -ne 'LSM' -and 
+                $_.PathName -notmatch 'OSE.EXE' -and $_.PathName -notmatch 'OSPPSVC.EXE' -and 
+                $_.PathName -notmatch 'Microsoft Security Client'
+            }
+
+            $Results = @()
+            ForEach ($svc in $NonDefaultServices) {
+                $Results += [pscustomobject]@{
+                    DisplayName = $svc.DisplayName
+                    Executable  = $svc.PathName
+                    StartMode   = $svc.StartMode
+                    StartName   = $svc.StartName
+                    State       = $svc.State
+                    Status      = $svc.Status
+                    Started     = $svc.Started
+                    Description = $svc.Description
+                }
+            }
+            $Results
+        }
+    } Else {
+        $NonDefaultServices = Get-wmiobject -Class Win32_Service | Where-Object {
+            $_.Caption -notmatch 'Windows' -and $_.PathName -notmatch 'Windows' -and 
+            $_.PathName -notmatch 'policyhost.exe' -and $_.Name -ne 'LSM' -and 
+            $_.PathName -notmatch 'OSE.EXE' -and $_.PathName -notmatch 'OSPPSVC.EXE' -and 
+            $_.PathName -notmatch 'Microsoft Security Client'
+        }
+
+        $Results = @()
+        ForEach ($svc in $NonDefaultServices) {
+            $Results += [pscustomobject]@{
+                DisplayName = $svc.DisplayName # Service Display Name (full name)
+                Executable  = $svc.PathName    # Service Executable
+                StartMode   = $svc.StartMode   # Service Startup mode
+                StartName   = $svc.StartName   # Service RunAs Account
+                State       = $svc.State       # Service State (running/stopped etc)
+                Status      = $svc.Status      # Service Status
+                Started     = $svc.Started     # Service Started status
+                Description = $svc.Description # Service Description
+            }
+        }
+        $toReturn = $Results
+    }
+    return $toReturn
+}
+Get-NonWinSvc | Out-GridView
