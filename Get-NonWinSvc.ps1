1e81e5cbc59ad2f91eedddc49295690b3c9a1ae5
neztach <36271369+neztach@users.noreply.github.com>
2022-12-02 17:54:23 -0600

    Update Get-NonWinSvc.ps1
    took function out to make the file able to be run stand-alone

--- Get-NonWinSvc.ps1
+++ Get-NonWinSvc.ps1
@@ -1,32 +1,7 @@
 ### Get Non-Windows Services
-Function Get-NonWinSvc {
-    Param ($CN = 'localhost')
-
-    If ($CN) {
-        $toReturn = Invoke-Command -ComputerName $CN -ScriptBlock {
-            $NonDefaultServices = Get-wmiobject -Class Win32_Service | Where-Object {
-                $_.Caption -notmatch 'Windows' -and $_.PathName -notmatch 'Windows' -and 
-                $_.PathName -notmatch 'policyhost.exe' -and $_.Name -ne 'LSM' -and 
-                $_.PathName -notmatch 'OSE.EXE' -and $_.PathName -notmatch 'OSPPSVC.EXE' -and 
-                $_.PathName -notmatch 'Microsoft Security Client'
-            }
-
-            $Results = @()
-            ForEach ($svc in $NonDefaultServices) {
-                $Results += [pscustomobject]@{
-                    DisplayName = $svc.DisplayName
-                    Executable  = $svc.PathName
-                    StartMode   = $svc.StartMode
-                    StartName   = $svc.StartName
-                    State       = $svc.State
-                    Status      = $svc.Status
-                    Started     = $svc.Started
-                    Description = $svc.Description
-                }
-            }
-            $Results
-        }
-    } Else {
+Param ([string]$CN)
+If ($CN) {
+    $toReturn = Invoke-Command -ComputerName $CN -ScriptBlock {
         $NonDefaultServices = Get-wmiobject -Class Win32_Service | Where-Object {
             $_.Caption -notmatch 'Windows' -and $_.PathName -notmatch 'Windows' -and 
             $_.PathName -notmatch 'policyhost.exe' -and $_.Name -ne 'LSM' -and 
@@ -37,18 +12,39 @@ Function Get-NonWinSvc {
         $Results = @()
         ForEach ($svc in $NonDefaultServices) {
             $Results += [pscustomobject]@{
-                DisplayName = $svc.DisplayName # Service Display Name (full name)
-                Executable  = $svc.PathName    # Service Executable
-                StartMode   = $svc.StartMode   # Service Startup mode
-                StartName   = $svc.StartName   # Service RunAs Account
-                State       = $svc.State       # Service State (running/stopped etc)
-                Status      = $svc.Status      # Service Status
-                Started     = $svc.Started     # Service Started status
-                Description = $svc.Description # Service Description
+                DisplayName = $svc.DisplayName
+                Executable  = $svc.PathName
+                StartMode   = $svc.StartMode
+                StartName   = $svc.StartName
+                State       = $svc.State
+                Status      = $svc.Status
+                Started     = $svc.Started
+                Description = $svc.Description
             }
         }
-        $toReturn = $Results
+        $Results
     }
-    return $toReturn
+} Else {
+    $NonDefaultServices = Get-wmiobject -Class Win32_Service | Where-Object {
+        $_.Caption -notmatch 'Windows' -and $_.PathName -notmatch 'Windows' -and 
+        $_.PathName -notmatch 'policyhost.exe' -and $_.Name -ne 'LSM' -and 
+        $_.PathName -notmatch 'OSE.EXE' -and $_.PathName -notmatch 'OSPPSVC.EXE' -and 
+        $_.PathName -notmatch 'Microsoft Security Client'
+    }
+
+    $Results = @()
+    ForEach ($svc in $NonDefaultServices) {
+        $Results += [pscustomobject]@{
+            DisplayName = $svc.DisplayName # Service Display Name (full name)
+            Executable  = $svc.PathName    # Service Executable
+            StartMode   = $svc.StartMode   # Service Startup mode
+            StartName   = $svc.StartName   # Service RunAs Account
+            State       = $svc.State       # Service State (running/stopped etc)
+            Status      = $svc.Status      # Service Status
+            Started     = $svc.Started     # Service Started status
+            Description = $svc.Description # Service Description
+        }
+    }
+    $toReturn = $Results
 }
-Get-NonWinSvc | Out-GridView
+return $toReturn
